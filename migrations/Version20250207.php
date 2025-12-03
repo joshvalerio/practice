55d1e08137039fde06cf2c3c72ac9ba8646efd87
joshvalerio <joshvalerio420@gmail.com>
2025-12-03 17:37:13 +0800

    practice

---
+++ migrations/Version20250207.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250207000000 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('
+            CREATE TABLE product (
+                id INT AUTO_INCREMENT NOT NULL,
+                name VARCHAR(128) NOT NULL,
+                description LONGTEXT NOT NULL,
+                size INT NOT NULL,
+                PRIMARY KEY(id)
+            ) DEFAULT CHARACTER SET utf8mb4 
+              COLLATE `utf8mb4_unicode_ci` 
+              ENGINE = InnoDB;
+        ');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE product');
+    }
+}
