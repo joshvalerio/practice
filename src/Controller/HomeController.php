4187a725f255b37e49fa2b8710ac6a7344a765cf
joshvalerio <joshvalerio420@gmail.com>
2025-12-05 18:16:29 +0800

    login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,9 +7,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/home', name: 'home') ]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
